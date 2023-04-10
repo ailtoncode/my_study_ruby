893c90212b01cc45ee688b43a5b0792557f2e5d0
ailtoncode <contato.sc17@gmail.com>
2023-04-10 09:09:25 -0300

    Testes adicionais

--- lib/hash.rb
+++ lib/hash.rb
@@ -8,3 +8,12 @@ grades = Hash.new
 grades["Jane Doe"] = 15
 
 puts grades["Jane Doe"]
+
+address = [
+    "Rua" => "Test test test",
+    "Num" => "80"
+]
+
+test = { "Ailton" => 2023, "address" => address}
+grades["Jane Doe"] = test
+p grades["Jane Doe"]["address"][0]["Rua"]
