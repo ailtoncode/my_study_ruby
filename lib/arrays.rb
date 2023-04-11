5aeabb09d782a9b1b5666d03d44f6ddc73551374
ailtoncode <contato.sc17@gmail.com>
2023-04-11 08:57:13 -0300

    array.find_index

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -10,50 +10,14 @@ list[0] = list[0].capitalize
 list[1] = list[1].capitalize
 list[2] = list[2].capitalize
 p list
-
-for i in 0..1 do
-    if i == 0
-        puts "nome: #{list[0]}"
-    else
-        puts "Sobrenome: #{list[1]} #{list[2]}"
-    end
-end
 puts '-------------------------------------------'
 
-count = 0
-nome = nil
-while count < 3 do
-    if nome == nil
-        nome = ""
-    end
-    if count > 0
-        nome += " "
-    end
-    nome.concat(list[count])
-    count+= 1
+if list.include? "Ailton"
+    list.push("TestNomeCompleto")
+    puts list[3]
 end
-puts nome
-puts '-------------------------------------------'
 
-count = 0
-list = []
-while count == 0
-    puts "Digite seu nome:"
-    nome = gets.chomp
-    list << nome
-    puts "Otimo! Seu nome é #{nome}"
-    puts "Digite 0 para repetir ou 1 para finalizar"
-    option = gets.chomp
+#find_index
 
-    if option != "0" && option != "1"
-        puts "Opção inválida, repetindo operação.."
-    else
-        count = option.to_i
-    end
-
-    if count == 1
-        puts "saindo.."
-    end
-end
-p list
-puts '-------------------------------------------'
+index = list.find_index("Silva")
+puts "index: #{index}"
