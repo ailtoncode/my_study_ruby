2fb9b8d9ee50fd8632888016bbc6691cfa20044a
ailtoncode <contato.sc17@gmail.com>
2023-04-25 15:23:54 -0300

    praticando hash

--- lib/hash.rb
+++ lib/hash.rb
@@ -10,10 +10,15 @@ grades["Jane Doe"] = 15
 puts grades["Jane Doe"]
 
 address = [
-    "Rua" => "Test test test",
+    "Rua" => "Test address",
     "Num" => "80"
 ]
 
-test = { "Ailton" => 2023, "address" => address}
-grades["Jane Doe"] = test
-p grades["Jane Doe"]["address"][0]["Rua"]
+#test = { "Ailton" => 2023, "address" => address}
+h = { nome: 'Ailton', address: address }
+grades["Jane Doe"] = h
+p grades["Jane Doe"][:address][0]["Rua"]
+puts '--------------------------------------'
+p h.keys
+p h.values
+puts h.empty?
